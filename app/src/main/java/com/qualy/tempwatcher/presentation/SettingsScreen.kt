7481c90ba194018337b35c888216b7640bdf8a6c
Qualyyy <Jarne.QUALY@student.howest.be>
2026-07-17 21:26:22 +0200

    Update settings screen

--- app/src/main/java/com/qualy/tempwatcher/presentation/SettingsScreen.kt
+++ app/src/main/java/com/qualy/tempwatcher/presentation/SettingsScreen.kt
@@ -1,20 +1,26 @@
 package com.qualy.tempwatcher.presentation
 
-import androidx.compose.ui.tooling.preview.PreviewScreenSizes
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.ui.Alignment
+import androidx.wear.compose.foundation.lazy.ScalingLazyColumn
+import androidx.wear.compose.foundation.lazy.rememberScalingLazyListState
+import androidx.wear.compose.material3.Button
+import androidx.wear.compose.material3.ListHeader
 import androidx.wear.compose.material3.MaterialTheme
 import androidx.wear.compose.material3.Text
 import androidx.wear.compose.ui.tooling.preview.WearPreviewDevices
-import androidx.wear.compose.ui.tooling.preview.WearPreviewFontScales
 
 
+@WearPreviewDevices
 @Composable
 fun SettingsScreen() {
+
+    val listState = rememberScalingLazyListState()
 
     Box(
         modifier = Modifier
@@ -22,8 +28,51 @@ fun SettingsScreen() {
             .background(MaterialTheme.colorScheme.background),
         contentAlignment = Alignment.Center
     ) {
-        Text(
-            text = "Settings"
-        )
+        ScalingLazyColumn(
+            state = listState
+        ) {
+
+            item {
+                ListHeader {
+                    Text("Settings")
+                }
+            }
+
+            item {
+
+            }
+
+            item {
+                Button(
+                    onClick = {
+                        // Open IP input
+                    },
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Text(
+                        """
+                        IP Address
+                        192.168.0.246
+                        """.trimIndent()
+                    )
+                }
+            }
+
+            item {
+                Button(
+                    onClick = {
+                        // Open port input later
+                    },
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Text(
+                        """
+                    Port
+                    5208
+                    """.trimIndent()
+                    )
+                }
+            }
+        }
     }
 }
