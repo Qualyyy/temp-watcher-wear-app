0f9589d7ff51f844c5323918a95f31936d7259ab
Qualyyy <Jarne.QUALY@student.howest.be>
2026-08-19 20:06:11 +0200

    Update time only when needed

--- app/src/main/java/com/qualy/tempwatcher/presentation/TimeState.kt
+++ app/src/main/java/com/qualy/tempwatcher/presentation/TimeState.kt
@@ -17,8 +17,15 @@ fun rememberCurrentTime(): String {
         initialValue = LocalTime.now().format(formatter)
     ) {
         while (true) {
-            value = LocalTime.now().format(formatter)
-            delay(1_000)
+            val now = LocalTime.now()
+
+            value = now.format(formatter)
+
+            // Wait until the start of the next minute
+            val delayMillis =
+                (60 - now.second) * 1_000L - now.nano / 1_000_000L
+
+            delay(delayMillis)
         }
     }.value
 }
